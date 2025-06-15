d77814340ba661410b084338aac8a5e4fcdde26f
sergoutback <webart2013@gmail.com>
2025-06-15 14:10:27 +0400

    Adjust .gitignore to exclude Pods and Xcode cache

--- Pods/Target Support Files/Pods-ar-camera-objective-c-ar-camera-objective-cUITests/Pods-ar-camera-objective-c-ar-camera-objective-cUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ar_camera_objective_c_ar_camera_objective_cUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ar_camera_objective_c_ar_camera_objective_cUITests
-@end
--- Pods/Target Support Files/Pods-ar-camera-objective-c/Pods-ar-camera-objective-c-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ar_camera_objective_c : NSObject
-@end
-@implementation PodsDummy_Pods_ar_camera_objective_c
-@end
